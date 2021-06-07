e53ca843db548c129b7be8d8b407bbef07c9c1e5
Yana Nazaryan <YNazaryan@appline.ru>
2021-06-07 22:53:18 +0300

    создание двух переменных

---
+++ TaskWork02.java
@@ -0,0 +1,13 @@
+public class TaskWork02 {
+
+    public static void main(String[] args) {
+
+        double a = 123.456;
+
+        System.out.println( a );
+
+        byte b = 12;
+
+        System.out.print( b );
+    }
+}
