0c02d9749e2a33f6de0ace9dd94ebc3f482cb59f
Yana Nazaryan <YNazaryan@appline.ru>
2021-06-23 12:45:04 +0300

    Доработки 2 и 3

--- TaskWork02.java
+++ TaskWork02.java
@@ -1,13 +1,12 @@
 public class TaskWork02 {
-
     public static void main(String[] args) {
 
+        // Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел. Присвойте ей произвольное дробное значение.
         double a = 123.456;
+        System.out.println(a);
 
-        System.out.println( a );
-
-        byte b = 12;
-
-        System.out.print( b );
+        // Создайте переменную целочисленного типа с наименьшим диапазоном чисел. Присвойте  ей наибольшее возможное значение.
+        byte b = 127;
+        System.out.println(b);
     }
 }
